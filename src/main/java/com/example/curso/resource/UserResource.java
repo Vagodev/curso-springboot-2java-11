d52984179c32002200e06957db1bac4fe2b7b018
vagodev <oi@vago.dev>
2022-07-28 03:00:58 -0300

    Arrumando a estrutura do projeto

--- src/main/java/com/example/curso/resource/UserResource.java
+++ src/main/java/com/example/curso/resource/UserResource.java
@@ -1,4 +1,4 @@
-package resource;
+package com.example.curso.resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
