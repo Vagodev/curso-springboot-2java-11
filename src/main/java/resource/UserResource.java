1bf803cb5566f8729197e3c849d60b1f88607a11
vagodev <oi@vago.dev>
2022-07-28 02:56:47 -0300

    User entity and resource

---
+++ src/main/java/resource/UserResource.java
@@ -0,0 +1,20 @@
+package resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+  
+  @GetMapping
+  public ResponseEntity<User> findAll(){
+    User u = new User(1L, "Victor", "victor@victor.com", "12345678", "12345");
+    return ResponseEntity.ok().body(u);
+
+
+  }
+}
